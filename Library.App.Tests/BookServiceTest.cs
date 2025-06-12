f3e72c894f04577f84ab4147e169ec7b79a08c13
Grosu2000 <sasha.grosu3@gmail.com>
2025-06-12 12:36:37 +0300

    unit-test

--- Library.App.Tests/BookServiceTest.cs
+++ Library.App.Tests/BookServiceTest.cs
@@ -15,7 +15,53 @@ public class BookServiceTests
         Assert.Equal("C# in Depth", book!.Title);
     }
 
-    // TODO: implement Should_BorrowBookSuccessfully
-    // TODO: implement Should_ReturnBookSuccessfully
-    // TODO: implement Should_ReturnAvailableBooks
+
+    [Fact]
+        // TODO: implement Should_BorrowBookSuccessfully
+
+    public void Should_BorrowBookSuccessfully()
+    {
+        var service = new BookService();
+        service.AddBook("CLR via C#");
+
+        var result = service.BorrowBook("CLR via C#");
+
+        Assert.True(result);
+        var book = service.FindBook("CLR via C#");
+        Assert.True(book!.IsBorrowed);
+    }
+
+    [Fact]
+        // TODO: implement Should_ReturnBookSuccessfully
+
+    public void Should_ReturnBookSuccessfully()
+    {
+        var service = new BookService();
+        service.AddBook("Effective C#");
+        service.BorrowBook("Effective C#");
+
+        var result = service.ReturnBook("Effective C#");
+
+        Assert.True(result);
+        var book = service.FindBook("Effective C#");
+        Assert.False(book!.IsBorrowed);
+    }
+
+    [Fact]
+        // TODO: implement Should_ReturnAvailableBooks
+
+    public void Should_ReturnAvailableBooks()
+    {
+        var service = new BookService();
+        service.AddBook("Book 1");
+        service.AddBook("Book 2");
+        service.AddBook("Book 3");
+        service.BorrowBook("Book 2");
+
+        var availableBooks = service.GetAvailableBooks();
+
+        Assert.Contains(availableBooks, b => b.Title == "Book 2");
+        Assert.DoesNotContain(availableBooks, b => b.Title == "Book 1");
+        Assert.DoesNotContain(availableBooks, b => b.Title == "Book 3");
+    }
 }
