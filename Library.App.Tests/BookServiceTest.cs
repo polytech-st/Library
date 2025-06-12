e30e8b80754b1168410e5f6c96caa4aa7cd4c9d5
Rekuw <maxduleba28@gmail.com>
2025-06-12 23:01:56 +0300

    Unit tests

--- Library.App.Tests/BookServiceTest.cs
+++ Library.App.Tests/BookServiceTest.cs
@@ -2,20 +2,62 @@ namespace Library.App.Tests;
 
 public class BookServiceTests
 {
-
+    public readonly string cid = "C# in Depth";
+    public readonly string tb = "Test Book";
     [Fact]
     public void Should_AddBook()
     {
         var service = new BookService();
-        service.AddBook("C# in Depth");
+        service.AddBook(cid);
 
-        var book = service.FindBook("C# in Depth");
+        var book = service.FindBook(cid);
 
         Assert.NotNull(book);
-        Assert.Equal("C# in Depth", book!.Title);
+        Assert.Equal(cid, book!.Title);
     }
 
-    // TODO: implement Should_BorrowBookSuccessfully
-    // TODO: implement Should_ReturnBookSuccessfully
-    // TODO: implement Should_ReturnAvailableBooks
+    [Fact]
+    public void Should_BorrowBookSuccessfully()
+    {
+        var service = new BookService();
+        service.AddBook(tb);
+
+        var result = service.BorrowBook(tb);
+
+        var book = service.FindBook(tb);
+
+        Assert.True(result);
+        Assert.True(book!.IsBorrowed);
+    }
+
+    [Fact]
+    public void Should_ReturnBookSuccessfully()
+    {
+        var service = new BookService();
+        service.AddBook(tb);
+        service.BorrowBook(tb);
+
+        var result = service.ReturnBook(tb);
+
+        var book = service.FindBook(tb);
+
+        Assert.True(result);
+        Assert.False(book!.IsBorrowed);
+    }
+
+    [Fact]
+    public void Should_ReturnAvailableBooks()
+    {
+        var service = new BookService();
+        service.AddBook("Book 1");
+        service.AddBook("Book 2");
+        service.AddBook("Book 3");
+        service.BorrowBook("Book 1");
+
+        var availableBooks = service.GetAvailableBooks();
+
+        Assert.Contains(availableBooks, b => b.Title == "Book 1");
+        Assert.DoesNotContain(availableBooks, b => b.Title == "Book 2");
+        Assert.DoesNotContain(availableBooks, b => b.Title == "Book 3");
+    }
 }
