eaed745257b6a52e7c3b3427e8385c92c88d0729
JabkoNasatasiia <nairobi.ahs@gmail.com>
2025-06-13 10:06:53 +0300

    Unit tests

--- Library.App.Tests/BookServiceTest.cs
+++ Library.App.Tests/BookServiceTest.cs
@@ -18,4 +18,48 @@ public class BookServiceTests
     // TODO: implement Should_BorrowBookSuccessfully
     // TODO: implement Should_ReturnBookSuccessfully
     // TODO: implement Should_ReturnAvailableBooks
+    [Fact]
+    public void Should_BorrowBookSuccessfully()
+    {
+        var service = new BookService();
+        service.AddBook("C# in Depth");
+
+        var borrowSuccess = service.BorrowBook("C# in Depth");
+
+        Assert.True(borrowSuccess);
+        var book = service.FindBook("C# in Depth");
+        Assert.NotNull(book);
+        Assert.True(book!.IsBorrowed);
+    }
+
+    [Fact]
+    public void Should_ReturnBookSuccessfully()
+    {
+        var service = new BookService();
+        service.AddBook("C# in Depth");
+        service.BorrowBook("C# in Depth");
+
+        var returnSuccess = service.ReturnBook("C# in Depth");
+
+        Assert.True(returnSuccess);
+        var book = service.FindBook("C# in Depth");
+        Assert.NotNull(book);
+        Assert.False(book!.IsBorrowed);
+    }
+
+    [Fact]
+    public void Should_ReturnAvailableBooks()
+    {
+        var service = new BookService();
+        service.AddBook("C# in Depth");
+        service.AddBook("Clean Code");
+
+        service.BorrowBook("C# in Depth");
+
+        var availableBooks = service.GetAvailableBooks();
+
+        Assert.Single(availableBooks);
+        Assert.Equal("C# in Depth", availableBooks[0].Title);
+    }
+
 }
